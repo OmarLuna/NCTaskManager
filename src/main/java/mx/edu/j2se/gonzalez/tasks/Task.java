632f4c0ce1526a7108e1d9a95c74f4464d03edd9
OmarLuna <omar1_antonio1@hotmail.com>
2021-11-14 20:31:36 -0600

    Added Java Documentation

--- src/main/java/mx/edu/j2se/gonzalez/tasks/Task.java
+++ src/main/java/mx/edu/j2se/gonzalez/tasks/Task.java
@@ -7,10 +7,23 @@ public class Task {
     int end=-1;
     int interval=-1;
     boolean active=false;
+
+    /**
+     *
+     * @param title Title of the task
+     * @param time The time the task will be run
+     */
     public Task(String title, int time){
         this.title=title;
         this.time=time;
     }
+
+    /**
+     * @param title Title of the task
+     * @param start Time when teh task starts
+     * @param end Time when the task ends
+     * @param interval The intervals the task should be repeated
+     */
     public Task(String title, int start, int end, int interval){
         this.title=title;
         this.start=start;
@@ -30,7 +43,7 @@ public class Task {
         this.active=active;
     }
     public int getTime(){
-        return time==-1?start:time;
+        return (time==-1)?start:time;
     }
     public void setTime(int time){
         if(this.time ==-1)
@@ -39,14 +52,22 @@ public class Task {
             this.time=time;
     }
     public int getStartTime(){
-        return start==-1?time:start;
+        return (start==-1)?time:start;
     }
     public int getEndTime(){
-        return end==-1?time:end;
+        return (end==-1)?time:end;
     }
     public int getRepeatInterval(){
-        return interval==-1?0:interval;
+        return (interval==-1)?0:interval;
     }
+
+    /**
+     * If the task wasn't a repetitive task executing this method will change it to
+     * a repetitive task
+     * @param start The time the task starts
+     * @param end The time the task ends
+     * @param interval The interval of the task
+     */
     public void setTime(int start, int end, int interval){
         this.time=-1;
         this.start=start;
@@ -56,6 +77,14 @@ public class Task {
     public boolean isRepeated(){
         return time == -1;
     }
+
+    /**
+     * Calculates the time for the next task
+     * @param current The current time
+     * @return Returns the next start time of the task execution after the current time.
+     * If after the specified time the task is not executed
+     * anymore, the method will return -1
+     */
     public int nextTimeAfter(int current){
         int i=start;
         while(i<end){
